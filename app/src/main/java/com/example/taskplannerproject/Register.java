dc368803eda943b61e646578ef7b410758abb9ce
gamer-12748 <aayushgarg2006@gmail.com>
2021-07-17 16:34:16 +0530

    added a lot of things, hope it works

--- app/src/main/java/com/example/taskplannerproject/Register.java
+++ app/src/main/java/com/example/taskplannerproject/Register.java
@@ -1,6 +1,5 @@
 package com.example.taskplannerproject;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,14 +12,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
@@ -49,48 +42,39 @@ public class Register extends AppCompatActivity {
         // do the actual registration using firebase.
         progressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(m_email, m_password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser f_user = mAuth.getCurrentUser();
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        FirebaseUser f_user = mAuth.getCurrentUser();
 
-                            Map<String, Object> user = new HashMap<>();
-                            user.put("name", m_name);
-                            user.put("email", m_email);
-                            user.put("date_created", new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date()));
-                            user.put("time_created", new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date()));
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("name", m_name);
+                        user.put("email", m_email);
+                        user.put("date_created", new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date()));
+                        user.put("time_created", new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date()));
 
-                            // Add a new document with a generated ID
-                            db.collection("users").document(f_user.toString())
-                                    .set(user)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void aVoid) {
-                                            Log.d(TAG, "DocumentSnapshot successfully written!");
-                                            progressBar.setVisibility(View.GONE);
-                                            startActivity(new Intent(Register.this, HomeScreen.class));
-                                            finish();
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Log.w(TAG, "Error writing document", e);
-                                            progressBar.setVisibility(View.GONE);
-                                            Toast.makeText(Register.this, "Authentication failed. Please check network and try again.",Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
+                        // Add a new document with a generated ID
+                        db.collection("users").document(f_user.toString())
+                                .set(user)
+                                .addOnSuccessListener(aVoid -> {
+                                    Log.d(TAG, "DocumentSnapshot successfully written!");
+                                    progressBar.setVisibility(View.GONE);
+                                    startActivity(new Intent(Register.this, HomeScreen.class));
+                                    finish();
+                                })
+                                .addOnFailureListener(e -> {
+                                    Log.w(TAG, "Error writing document", e);
+                                    progressBar.setVisibility(View.GONE);
+                                    Toast.makeText(Register.this, "Authentication failed. Please check network and try again.",Toast.LENGTH_SHORT).show();
+                                });
 
-                            Log.d(TAG, "createUserWithEmail:success");
+                        Log.d(TAG, "createUserWithEmail:success");
 
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            progressBar.setVisibility(View.GONE);
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(Register.this, "Authentication failed. Please check network and try again.",Toast.LENGTH_SHORT).show();
-                        }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        progressBar.setVisibility(View.GONE);
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(Register.this, "Authentication failed. Please check network and try again.",Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -101,41 +85,33 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
-        progressBar = (ProgressBar) findViewById(R.id.register_progressBar);
+        progressBar = findViewById(R.id.register_progressBar);
         //component initializations
-        name = (EditText) findViewById(R.id.register_name);
-        email = (EditText) findViewById(R.id.register_email);
-        password = (EditText) findViewById(R.id.register_password);
-        confirmPassword = (EditText) findViewById(R.id.register_confirmPassword);
-        switchPage = (TextView) findViewById(R.id.register_switchPageToLogin);
-        registerSubmission = (Button) findViewById(R.id.register_submission);
+        name = findViewById(R.id.register_name);
+        email = findViewById(R.id.register_email);
+        password = findViewById(R.id.register_password);
+        confirmPassword = findViewById(R.id.register_confirmPassword);
+        switchPage = findViewById(R.id.register_switchPageToLogin);
+        registerSubmission = findViewById(R.id.register_submission);
         db = FirebaseFirestore.getInstance();
-        switchPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Register.this, Login.class));
-            }
-        });
+        switchPage.setOnClickListener(v -> startActivity(new Intent(Register.this, Login.class)));
 
-        registerSubmission.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String s_name = name.getText().toString();
-                String s_email = email.getText().toString();
-                String s_password = password.getText().toString();
-                String s_confirmPassword = confirmPassword.getText().toString();
-                if (s_name.isEmpty() || s_email.isEmpty() || s_password.isEmpty() || s_confirmPassword.isEmpty()) {
-                    Toast.makeText(Register.this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
-                } else if (!s_password.equals(s_confirmPassword)) {
-                    Toast.makeText(Register.this, "Passwords do not match, please Check passwords", Toast.LENGTH_SHORT).show();
-                } else if (password.length() < 6) {
-                    Toast.makeText(Register.this, "Password cannot be less than 6 digits", Toast.LENGTH_SHORT).show();
-                } else if (!VALID_EMAIL_ADDRESS_REGEX.matcher(s_email).matches()) {
-                    Toast.makeText(Register.this, "Email in wrong format", Toast.LENGTH_SHORT).show();
-                } else {
-                    registerUser(s_name, s_email, s_password);
-                }
+        registerSubmission.setOnClickListener(v -> {
+            String s_name = name.getText().toString();
+            String s_email = email.getText().toString();
+            String s_password = password.getText().toString();
+            String s_confirmPassword = confirmPassword.getText().toString();
+            if (s_name.isEmpty() || s_email.isEmpty() || s_password.isEmpty() || s_confirmPassword.isEmpty()) {
+                Toast.makeText(Register.this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
+            } else if (!s_password.equals(s_confirmPassword)) {
+                Toast.makeText(Register.this, "Passwords do not match, please Check passwords", Toast.LENGTH_SHORT).show();
+            } else if (password.length() < 6) {
+                Toast.makeText(Register.this, "Password cannot be less than 6 digits", Toast.LENGTH_SHORT).show();
+            } else if (!VALID_EMAIL_ADDRESS_REGEX.matcher(s_email).matches()) {
+                Toast.makeText(Register.this, "Email in wrong format", Toast.LENGTH_SHORT).show();
+            } else {
+                registerUser(s_name, s_email, s_password);
             }
         });
     }
-};
+}
