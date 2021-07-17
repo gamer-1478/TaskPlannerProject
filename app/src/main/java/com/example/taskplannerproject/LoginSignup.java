fd936b4f1797c65de8f244605f19530bea71f3ee
gamer-12748 <aayushgarg2006@gmail.com>
2021-07-17 17:22:07 +0530

    commit 5

--- app/src/main/java/com/example/taskplannerproject/LoginSignup.java
+++ app/src/main/java/com/example/taskplannerproject/LoginSignup.java
@@ -4,20 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 public class LoginSignup extends AppCompatActivity {
-
-    private Button login, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
-        login = findViewById(R.id.loginSignup_login);
-        signup = findViewById(R.id.loginSignup_signup);
+        Button login = findViewById(R.id.loginSignup_login);
+        Button signup = findViewById(R.id.loginSignup_signup);
 
         login.setOnClickListener(v -> startActivity(new Intent(LoginSignup.this, Login.class)));
         signup.setOnClickListener(v -> startActivity(new Intent(LoginSignup.this, Register.class)));
